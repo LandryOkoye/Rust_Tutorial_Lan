ccf7b567956f24485223a7dc2d5721f0d4a89e0f
Landry <Landryokoye373@gmail.com>
2026-06-26 17:39:25 +0100

    Done w guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
-use std::ops::ControlFlow::Break;
+use colored::*;
 
 fn main() {
     println!("Hello, world!");
@@ -14,7 +14,7 @@ fn main() {
     // ------- Guessing Game with Let's Get Rusty!!------------------------------------------
 
     // Generate random secret number
-    let secret_number = rand::thread_rng().gen_range(1, 10);
+    let secret_number = rand::thread_rng().gen_range(1, 20);
     print!("The secret number is = {}", secret_number);
 
     loop {
@@ -25,19 +25,20 @@ fn main() {
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-        let guess: i32 = guess
-            .trim()
-            .parse()
-            .expect("Failed to parse input, please input a number");
+
+        let guess: i32 =  match guess.trim().parse(){
+                Ok(num ) => num,
+                Err(_) => continue
+            };
 
         println!("You guessed - {}", guess);
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
+            Ordering::Less => println!("{}", "Too small!".red()),
             Ordering::Equal => {
-                println!("You win!!"); 
+                println!("{}", "You win!!".green()); 
                 break;
             }  // This breaks the loop when the correct answer is provided.
-            Ordering::Greater => println!("Too big!")
+            Ordering::Greater => println!("{}", "Too big!".red())
             
         }
         println!("The correct answer is - {}", secret_number);
